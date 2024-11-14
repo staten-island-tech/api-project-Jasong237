dead5d6643a90abcf5e74fdd2c42947b18e5490d
Jasong237 <jasonguo4635@gmail.com>
2024-11-14 13:58:47 -0500

    yhtjuykj

--- app/main.js
+++ app/main.js
@@ -1,1 +1,28 @@
 import "./style.css";
+// get data
+// promises
+//show data
+
+async function getData() {
+  try {
+    //returns a promise
+    const response = await fetch("https://valorant-api.com/v1/agents");
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //converst response to json
+      const data = await response.json();
+      console.log(data.data);
+      //unique to this api
+      data.data
+        .forEach((agent) => console.log(agent.displayName))
+        .document.querySelector("div")
+        .insertAdjacentHTML("afterbegin", `<h1> ${agent.displayName}</h1>`);
+    }
+  } catch (error) {
+    alert("hey I could not find that agent");
+  }
+}
+
+getData();
