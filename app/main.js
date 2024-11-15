e7913d8e99ea9721b1ec704caa2fd4acfe646fef
Jasong237 <jasonguo4635@gmail.com>
2024-11-15 13:59:11 -0500

    egrtgetg

--- app/main.js
+++ app/main.js
@@ -2,11 +2,13 @@ import "./style.css";
 // get data
 // promises
 //show data
-
+const DOMSelectors = {
+  container: document.querySelector(".container"),
+};
 async function getData() {
   try {
     //returns a promise
-    const response = await fetch("https://valorant-api.com/v1/agents");
+    const response = await fetch("https://api.disneyapi.dev/character");
     //guard clause
     if (response.status != 200) {
       throw new Error(response);
@@ -15,10 +17,12 @@ async function getData() {
       const data = await response.json();
       console.log(data.data);
       //unique to this api
-      data.data
-        .forEach((agent) => console.log(agent.displayName))
-        .document.querySelector("div")
-        .insertAdjacentHTML("afterbegin", `<h1> ${agent.displayName}</h1>`);
+      data.data.forEach((show) => console.log(show.films));
+
+      DOMSelectors.container.insertAdjacentHTML(
+        "afterbegin",
+        `<h1> ${show.films}</h1>`
+      );
     }
   } catch (error) {
     alert("hey I could not find that agent");
@@ -26,3 +30,6 @@ async function getData() {
 }
 
 getData();
+
+// test fetch call to get data
+// if coors is yes try again?
